afcc737ec752f6dbb7af24acc6d5492f3f076c1f
httaX996 <damindakule@gmail.com>
2025-10-24 17:08:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "8DsBXKSS#7HQkE_QB1F91sZyMoRZsYv0jx9Y8JDzHZxprrFYKsk8",
+SESSION_ID: process.env.SESSION_ID || "dMowGQBb#3PBJk2ffFNLh_DkOiNlQXjkBJWO8erffCVrp3nzVGIU",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
