3d3c4051e88f73b27b37038af33d391db926754a
httaX996 <damindakule@gmail.com>
2025-12-12 10:04:02 +0530

    Update contact display name in ckxx.js

--- plugins/ckxx.js
+++ plugins/ckxx.js
@@ -130,7 +130,7 @@ const ck = {
     },
     message: {
         contactMessage: {
-            displayName: "〴ᴄʜᴇᴛʜᴍɪɴᴀ ᴋᴀᴠɪꜱʜᴀɴ ×͜×",
+            displayName: "〴ᴄʜᴇᴛʜᴍɪɴᴀ ×͜×",
             vcard: `BEGIN:VCARD
 VERSION:3.0
 FN:Meta
